8756bb6fb9aa679f18a2e33046407236ad531f5c
zahra <zahramohammadimehr04@gmail.com>
2023-07-03 23:37:55 +0330

    write link shortener model

--- url/models.py
+++ url/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class LinkShortenerModel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    orginal_url = models.URLField(max_length=1000, null=False, blank=False)
+    short_url = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    url_viewed = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.short_url
+    
+    def get_absolute_url(self):
+        return self.orginal_url
+
+
+  
